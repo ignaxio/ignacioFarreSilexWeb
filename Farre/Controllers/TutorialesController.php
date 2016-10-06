ce43b2b05fb0a10dc21bc9e7d2234c2b39884570
Ignacio Indra <ignaciofarrefuentes@gmail.com>
2016-10-06 11:30:22 +0200

    puesta al dia

--- Farre/Controllers/TutorialesController.php
+++ Farre/Controllers/TutorialesController.php
@@ -21,12 +21,19 @@ class TutorialesController {
 
     //Tenemos que pedir los datos de la categoría y de los tutoriales de esa categoría
     $categoria = $this->drupalData->getCategoriaDeLosTutoriales($categoriaId);
-//    $tutoriales = $this->drupalData->getTutorialesDeUnaCategoria($categoria);
-//    print_r($this->drupalData->getTutorialesDeUnaCategoria($categoria));
+    $tutoriales = $this->drupalData->getTutorialesDeUnaCategoria($categoriaId);
+    $tutorialesFinales = [];
     //Para los tutoriales hay que transformar las fechas a formatos válidos.
-//    foreach ($tutoriales as $key => $tutorial) {
-//      
-//    }
+    foreach ($tutoriales as $key => $tutorial) {      
+      $tutorial = [
+          'titulo' => $tutorial->title[0]->value,
+          'textoResumen' => $tutorial->field_tutorial_subtitulo[0]->value,
+          'imagen' => $tutorial->field_image,
+          'url' => $tutorial->path,
+          'fechaPublicacion' => date("d-m-Y", $tutorial->created[0]->value),
+              ];
+      $tutorialesFinales[$key] = $tutorial;
+    }
 
     //print_r($categoria[0]->field_categoria_titulo_desc[0]->value);
     //Arrays de datos para mandar a la vista
@@ -36,9 +43,8 @@ class TutorialesController {
             'subtitulo' => $categoria[0]->field_categoria_titulo_desc[0]->value,
             'imagen' => $categoria[0]->field_categoria_imagen[0],
             'texto' => $categoria[0]->description[0]->value,
-            'miTlf' => '+34 627190899',
         ],
-        'tutoriales' => 'tuto',
+        'tutoriales' => $tutorialesFinales,
     ];
     return $app['twig']->render('categoria-de-los-tutoriales.twig', $datos);
   }
--- Farre/Services/DrupalData.php
+++ Farre/Services/DrupalData.php
@@ -1,66 +1,61 @@
 <?php
+
 namespace Farre\Service;
 
 class DrupalData {
+
   private $basePath;
-  
+
   public function __construct($basePath) {
     $this->basePath = $basePath;
   }
 
   private function getProxy() {
-    // Edit the four values below
-    $PROXY_HOST = "proxy.indra.es"; // Proxy server address
-    $PROXY_PORT = "8080";    // Proxy server port
-    $PROXY_USER = "ifarre";    // Username
-    $PROXY_PASS = "ClaveV2-";   // Password
-    // Username and Password are required only if your proxy server needs basic authentication
+    if ($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
+      // Edit the four values below
+      $PROXY_HOST = "proxy.indra.es"; // Proxy server address
+      $PROXY_PORT = "8080";    // Proxy server port
+      $PROXY_USER = "ifarre";    // Username
+      $PROXY_PASS = "ClaveV2-";   // Password
+      // Username and Password are required only if your proxy server needs basic authentication
 
-    $auth = base64_encode("$PROXY_USER:$PROXY_PASS");
-    stream_context_set_default(
-            array(
-                'http' => array(
-                    'proxy' => "tcp://$PROXY_HOST:$PROXY_PORT",
-                    'request_fulluri' => true,
-                    'header' => "Proxy-Authorization: Basic $auth"
-                // Remove the 'header' option if proxy authentication is not required
-                )
-            )
-    );
+      $auth = base64_encode("$PROXY_USER:$PROXY_PASS");
+      stream_context_set_default(
+              array(
+                  'http' => array(
+                      'proxy' => "tcp://$PROXY_HOST:$PROXY_PORT",
+                      'request_fulluri' => true,
+                      'header' => "Proxy-Authorization: Basic $auth"
+                  // Remove the 'header' option if proxy authentication is not required
+                  )
+              )
+      );
+    }
   }
 
   public function getExperiencias() {
-    if($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
-      $this->getProxy();
-    }    
+    $this->getProxy();
     return json_decode(file_get_contents($this->basePath . '/get-experiencias'));
   }
-  
+
   public function getLibros() {
-    if($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
-      $this->getProxy();
-    }
+    $this->getProxy();
     return json_decode(file_get_contents($this->basePath . '/libros'));
   }
-  
+
   public function getCategoriasDeLosTutoriales() {
-    if($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
-      $this->getProxy();
-    }
+    $this->getProxy();
     return json_decode(file_get_contents($this->basePath . '/tutoriales-categorias'));
   }
+
   public function getCategoriaDeLosTutoriales($categoriaId) {
-    if($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
-      $this->getProxy();
-    }
+    $this->getProxy();
     return json_decode(file_get_contents($this->basePath . '/tutoriales-categorias/' . $categoriaId));
   }
+
   public function getTutorialesDeUnaCategoria($categoriaId) {
-    if($_SERVER['HTTP_HOST'] == 'ignaciofarre-silex.local') {
-      $this->getProxy();
-    }
+    $this->getProxy();
     return json_decode(file_get_contents($this->basePath . '/tutoriales/' . $categoriaId));
   }
 
 }
-
